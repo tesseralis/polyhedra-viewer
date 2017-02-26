acfb7a7ee0e9c014bbb68b83fea62eef1357468e
Nat Alison <tesseralis@gmail.com>
2017-02-25 18:52:24 -0800

    generate polyhedra

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import 'x3dom';
 import 'x3dom/x3dom.css';
 import logo from './logo.svg';
 import './App.css';
+import Polyhedron from './Polyhedron';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.solid = {
+      "name": "tetrahedron",
+      "vertices": [
+                  [0.0,0.0,1.732051],
+                  [1.632993,0.0,-0.5773503],
+                  [-0.8164966,1.414214,-0.5773503],
+                  [-0.8164966,-1.414214,-0.5773503]
+                ],
+      "edges": [[0,1],[0,2],[0,3],[1,2],[1,3],[2,3]],
+      "faces": [[0,1,2],[0,2,3],[0,3,1],[1,3,2]]
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -14,13 +30,8 @@ class App extends Component {
         </div>
         <x3d className="X3d">
           <scene>
-            <shape>
-              <appearance>
-                {/* http://stackoverflow.com/a/33860892 */}
-                <material is diffuseColor="1 0 0"></material>
-              </appearance>
-              <box></box>
-            </shape>
+            <viewpoint is position="0,0,5"></viewpoint>
+            <Polyhedron solid={this.solid} />
           </scene>
         </x3d>
       </div>
