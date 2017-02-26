7ab36478fda7e325c667c5e821869b4b83c98213
Nat Alison <tesseralis@gmail.com>
2017-02-26 15:55:22 -0800

    fix color issue i guess

--- src/Polyhedron.js
+++ src/Polyhedron.js
@@ -2,33 +2,34 @@ import React, { Component, PropTypes } from 'react';
 import { rgb } from 'd3-color';
 import { schemeSet1 } from 'd3-scale-chromatic';
 
+// TODO move this to a util file
+const joinListOfLists = (list, outerSep, innerSep) => {
+  return list.map(elem => elem.join(innerSep)).join(outerSep);
+}
+
+const toRgb = hex => {
+  const asRgb = rgb(hex);
+  return [asRgb.r, asRgb.g, asRgb.b].map(d => d/255);
+};
+
+const colors = schemeSet1.map(color => toRgb(color));
+
+const colorAttr = joinListOfLists(colors, ', ', ' ');
+
 class Polyhedron extends Component {
 
   static propTypes = {
     solid: PropTypes.object.isRequired
   }
 
-  // TODO move this to a util file
-  joinListOfLists(list, outerSep, innerSep) {
-    return list.map(elem => elem.join(innerSep)).join(outerSep);
-  }
-
-  getColor(n) {
+  getColorIndex(n) {
     const colorMap = { 3: 4, 4: 0, 5: 1, 6: 2, 8: 3, 10: 6 };
-    const asRgb = rgb(schemeSet1[colorMap[n]]);
-    return [asRgb.r, asRgb.g, asRgb.b].map(d => d/255);
-  }
-
-  renderColor(faces) {
-    const colorsPerFace = faces.map(face => this.getColor(face.length));
-    return (
-      <color is color={this.joinListOfLists(colorsPerFace, ', ', ' ')}></color>
-    );
+    return colorMap[n];
   }
 
   renderCoordinates(points) {
     return (
-      <coordinate is point={this.joinListOfLists(points, ', ', ' ')}></coordinate>
+      <coordinate is point={joinListOfLists(points, ', ', ' ')}></coordinate>
     );
   }
 
@@ -43,17 +44,18 @@ class Polyhedron extends Component {
           </appearance>
           <indexedfaceset is
             colorPerVertex="false"
-            coordindex={ this.joinListOfLists(faces, ' -1 ', ' ') }
+            colorindex={ faces.map(face => this.getColorIndex(face.length)).join(' ') }
+            coordindex={ joinListOfLists(faces, ' -1 ', ' ') }
           >
             { this.renderCoordinates(vertices) }
-            { this.renderColor(faces) }
+            <color is color={colorAttr}></color>
           </indexedfaceset>
         </shape>
         <shape>
           <indexedlineset is
-            coordindex={ this.joinListOfLists(edges, ' -1 ', ' ') }
+            coordindex={ joinListOfLists(edges, ' -1 ', ' ') }
           >
-            { this.renderCoordinates(this.props.solid.vertices) }
+            { this.renderCoordinates(vertices) }
           </indexedlineset>
         </shape>
       </group>
