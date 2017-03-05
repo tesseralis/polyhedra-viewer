39788e20a9cdd2c534994dca904083b970a666c1
Nat Alison <tesseralis@gmail.com>
2017-03-05 11:23:22 -0800

    reformat

--- src/Table.js
+++ src/Table.js
@@ -10,31 +10,31 @@ import styles from './TableStyles';
 
 const Table = () => {
   return (
-    <div className={css(styles.table)}>{
-      groups.map(group => (
-        <div key={group.name} className={css(styles.group)}>
-          <div className={css(styles.groupHeader)}>
-            <GroupHeader name={group.name} />
-          </div>
-          <p className={css(styles.groupDescription)}>{group.description}</p>
-          <div className={css(styles.list)}>{
-            group.polyhedra.map(polyhedronName => {
-              const escapedName = escapeName(polyhedronName);
-              const img = require(`./images/${escapedName}.png`);
-              return (
-                <Link
-                  key={polyhedronName}
-                  to={escapedName}
-                  className={css(styles.link, commonStyles.hover)}
-                >
-                  <img className={css(styles.image)} src={img} alt={polyhedronName}></img>
-                </Link>
-              )
-            })
-          }</div>
+    <div className={css(styles.table)}>
+    { groups.map(group => (
+      <div key={group.name} className={css(styles.group)}>
+        <div className={css(styles.groupHeader)}>
+          <GroupHeader name={group.name} />
         </div>
-      ))
-    }</div>
+        <p className={css(styles.groupDescription)}>{group.description}</p>
+        <div className={css(styles.list)}>
+        { group.polyhedra.map(polyhedronName => {
+          const escapedName = escapeName(polyhedronName);
+          const img = require(`./images/${escapedName}.png`);
+          return (
+            <Link
+              key={polyhedronName}
+              to={escapedName}
+              className={css(styles.link, commonStyles.hover)}
+            >
+              <img className={css(styles.image)} src={img} alt={polyhedronName}></img>
+            </Link>
+          )
+        }) }
+        </div>
+      </div>
+    )) }
+    </div>
   );
 };
 
