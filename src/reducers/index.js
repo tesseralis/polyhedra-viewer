2db2baa98e2f00df79aacc5a8790940cb76de955
Nat Alison <tesseralis@gmail.com>
2017-03-07 00:00:27 -0800

    no error on redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,13 @@
 import { combineReducers } from 'redux'
 
+const initialState = {}
+
+const reducer = (state = initialState, action) => {
+  return state
+}
+
 const rootReducer = combineReducers({
+  reducer
 })
 
 export default rootReducer
