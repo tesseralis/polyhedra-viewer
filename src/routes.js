828e80494503d4b353d69d8096eb6126a7c214e3
Nat Alison <tesseralis@gmail.com>
2017-03-08 10:39:04 -0800

    move app to containers

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Viewer from './containers/Viewer'
+import App from './containers/App'
 import Table from './components/Table'
-import App from './components/App'
 
 export default <Route path="/" component={App}>
   <IndexRoute component={Table} />
