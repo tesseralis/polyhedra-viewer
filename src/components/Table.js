cbfb68ba5169618c0535db7fe4cb262139eff45a
Nat Alison <tesseralis@gmail.com>
2017-03-09 22:21:28 -0800

    refactor table styles

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,38 +1,114 @@
 import React from 'react'
-import { css } from 'aphrodite/no-important'
+import { css, StyleSheet } from 'aphrodite/no-important'
 import { Link } from 'react-router'
 
 import { groups } from '../constants/polyhedra'
 import { escapeName } from '../util'
 import GroupHeader from './GroupHeader'
 
-import styles from './TableStyles'
+import { hoeflerText, andaleMono } from '../styles/fonts'
+import { fadeIn } from '../styles/animations'
+import { hover } from '../styles/common'
+
+// TODO don't hardcode this?
+const thumbnailSize = 100
+
+const PolyhedronLink = ({ name }) => {
+  const styles = StyleSheet.create({
+    link: {
+      ...hover,
+      width: thumbnailSize,
+      height: thumbnailSize,
+      display: 'flex',
+      justifyContent: 'center',
+      overflow: 'hidden',
+    },
+    
+    image: {
+      height: thumbnailSize,
+    },
+  })
+  const escapedName = escapeName(name)
+  const img = require(`../images/${escapedName}.png`)
+  return (
+    <Link to={'/' + escapedName} className={css(styles.link)}>
+      <img className={css(styles.image)} src={img} alt={name}></img>
+    </Link>
+  )
+}
+
+const PolyhedronList = ({ polyhedra }) => {
+  const maxThumbnailsPerLine = 7
+  const styles = StyleSheet.create({
+    list: {
+      display: 'flex',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+      maxWidth: maxThumbnailsPerLine * thumbnailSize,
+      margin: 'auto',
+    },
+  })
+
+  return (
+    <div className={css(styles.list)}>
+      { polyhedra.map(name => <PolyhedronLink key={name} name={name} />) }
+    </div>
+  )
+}
+
+const PolyhedronGroup = ({ name, description, polyhedra }) => {
+  const styles = StyleSheet.create({
+    group: {
+      margin: 18,
+      maxWidth: 1000,
+    },
+    
+    header: {
+      margin: '5px 0', // TODO figure out another notation for this?
+    },
+    
+    description: {
+      fontFamily: hoeflerText,
+      color: 'DimGrey',
+      margin: 14,
+      lineHeight: '22px',
+    },
+  })
+
+  return (
+    <div className={css(styles.group)}>
+      <GroupHeader name={name} styles={styles.header} />
+      <p className={css(styles.description)}>{description}</p>
+      <PolyhedronList polyhedra={polyhedra} />
+    </div>
+  )
+}
 
 const Table = () => {
+  const styles = StyleSheet.create({
+    table: {
+      width: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      textAlign: 'center',
+  
+      animationName: fadeIn,
+      animationDuration: '1s',
+    },
+  
+    title: {
+      padding: 32,
+      fontFamily: andaleMono,
+      fontSize: 36,
+      fontWeight: 'bold',
+    },
+  })
+
   return (
     <div className={css(styles.table)}>
       <h1 className={css(styles.title)}>Polyhedra Viewer</h1>
-      { groups.map(group => (
-        <div key={group.name} className={css(styles.group)}>
-          <GroupHeader name={group.name} styles={styles.groupHeader} />
-          <p className={css(styles.groupDescription)}>{group.description}</p>
-          <div className={css(styles.list)}>
-            { group.polyhedra.map(polyhedronName => {
-              const escapedName = escapeName(polyhedronName)
-              const img = require(`../images/${escapedName}.png`)
-              return (
-                <Link
-                  key={polyhedronName}
-                  to={'/' + escapedName}
-                  className={css(styles.link)}
-                >
-                  <img className={css(styles.image)} src={img} alt={polyhedronName}></img>
-                </Link>
-              )
-            }) }
-          </div>
-        </div>
-      )) }
+      { groups.map(group => <PolyhedronGroup key={group.name} {...group} />) }
     </div>
   )
 }
