d26e36209a959d64164e7226b8202d75ad2cee80
Nat Alison <tesseralis@gmail.com>
2017-03-11 19:30:18 -0800

    standardize naming

--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import { StyleSheet } from 'aphrodite/no-important'
+import _ from 'lodash'
 
 import X3dScene from '../components/X3dScene'
 import SidebarMenu from '../components/SidebarMenu'
 import ConfigMenu from '../components/ConfigMenu'
-import ViewerTitle from '../components/ViewerTitle'
+import Title from './Title'
+import { fixed } from '../styles/common'
+
+const styles = StyleSheet.create({
+  title: {
+    ...fixed('bottom', 'right'),
+    maxWidth: '50%',
+    textAlign: 'right',
+  }
+})
 
 const Viewer = ({ solid, polyhedron: Polyhedron, sidebar: Sidebar, configForm: ConfigForm }) => (
   <div>
@@ -12,7 +23,7 @@ const Viewer = ({ solid, polyhedron: Polyhedron, sidebar: Sidebar, configForm: C
     </X3dScene>
     <SidebarMenu sidebar={Sidebar} />
     <ConfigMenu configForm={ConfigForm} />
-    <ViewerTitle text={solid.name} />
+    <Title styles={styles.title}>{ _.capitalize(solid.name) }</Title>
   </div>
 )
 
