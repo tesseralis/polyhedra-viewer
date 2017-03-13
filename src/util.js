1ea53bb82392a9ea52eec0c58a4d7090dfda7771
Nat Alison <tesseralis@gmail.com>
2017-03-12 22:51:49 -0700

    add doc

--- src/util.js
+++ src/util.js
@@ -1,6 +1,8 @@
-// Lodash functions
 import _ from 'lodash'
 
+/**
+ * Create an object from teh array given the value and key iteratees.
+ */
 export const mapObject = (arr, valueIter=_.identity, keyIter=_.identity) => {
   keyIter = _.iteratee(keyIter)
   valueIter = _.iteratee(valueIter)
