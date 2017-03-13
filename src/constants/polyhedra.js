53392d6d74f3719086e081fe6c8f21f7ab2a1a9b
Nat Alison <tesseralis@gmail.com>
2017-03-12 23:36:24 -0700

    move jsol definitions to json file

--- src/constants/polyhedra.js
+++ src/constants/polyhedra.js
@@ -1,59 +1,15 @@
 import _ from 'lodash'
 
-const johnsonSubgroups = [
-  'pyramids',
-  'cupolæ and rotunda',
-  'elongated and gyroelongated pyramids',
-  'bipyramids',
-  'elongated cupolæ and rotundæ',
-  'bicupolæ',
-  'cupola-rotundæ and birotunda',
-  'elongated bicupolæ',
-  'elongated cupola-rotundæ and birotundæ',
-  'gyroelongated bicupolæ, cupola-rotunda, and birotunda',
-  'augmented triangular prisms',
-  'augmented pentagonal and hexagonal prisms',
-  'augmented dodecahedra',
-  'diminished icosahedra',
-  'augmented truncated tetrahedron and truncated cubes',
-  'augmented truncated dodecahedra',
-  'gyrate rhombicosidodecahedra',
-  'diminished rhombicosidodecahedra',
-  'snub antiprisms',
-  'others',
-]
-
-// TODO come up with a less janky way to store these
-const johnsonSubgroupIndices = {
-  'pyramids': 2,
-  'cupolæ and rotunda': 6,
-  'elongated and gyroelongated pyramids': 11,
-  'bipyramids': 17,
-  'elongated cupolæ and rotundæ': 25,
-  'bicupolæ': 31,
-  'cupola-rotundæ and birotunda': 34,
-  'elongated bicupolæ': 39,
-  'elongated cupola-rotundæ and birotundæ': 43,
-  'gyroelongated bicupolæ, cupola-rotunda, and birotunda': 48,
-  'augmented triangular prisms': 51,
-  'augmented pentagonal and hexagonal prisms': 57,
-  'augmented dodecahedra': 61,
-  'diminished icosahedra': 64,
-  'augmented truncated tetrahedron and truncated cubes': 67,
-  'augmented truncated dodecahedra': 71,
-  'gyrate rhombicosidodecahedra': 75,
-  'diminished rhombicosidodecahedra': 83,
-  'snub antiprisms': 85,
-  'others': 92,
-}
-
 const getPolyhedra = groupName => require(`../data/groups/${groupName}.json`)
 
+/* Johnson Solid Subgroups */
+const johnsonSolids = getPolyhedra('johnson')
+const johnsonSubgroups = require('../data/johnsonSubgroups.json')
+const getEndIndex = i => i === johnsonSubgroups.length - 1 ? 92 : johnsonSubgroups[i+1].index
 const getJohnsonPolyhedra = () => {
-  const johnsonSolids = getPolyhedra('johnson')
-  return johnsonSubgroups.map((subgroupName, i) => ({
-    name: subgroupName,
-    polyhedra: johnsonSolids.slice(i === 0 ? 0 : johnsonSubgroupIndices[johnsonSubgroups[i-1]], johnsonSubgroupIndices[subgroupName]),
+  return johnsonSubgroups.map(({ name, index }, i) => ({
+    name,
+    polyhedra: johnsonSolids.slice(index, getEndIndex(i))
   }))
 }
 
