05a390c60f4398da332cc76e60b2ee06b7cada89
Nat Alison <tesseralis@gmail.com>
2017-12-30 14:39:27 -0800

    yay! it kinda works

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
+
+import PeriodicTable from './components/PeriodicTable'
 import App from './containers/App'
 import Table from './containers/Table'
 import Viewer from './containers/Viewer'
 
 export default <Route path="/" component={App}>
   <IndexRoute component={Table} />
+  <Route path="table" component={PeriodicTable} />
   <Route path=":solid" component={Viewer} />
 </Route>
