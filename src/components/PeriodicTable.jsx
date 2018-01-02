98726a6d3d490cb23584ec4c07150a27cadba5a6
Nat Alison <tesseralis@gmail.com>
2018-01-01 23:09:51 -0800

    move table definitions to constants

--- src/components/PeriodicTable.jsx
+++ src/components/PeriodicTable.jsx
@@ -4,175 +4,18 @@ import { css, StyleSheet } from 'aphrodite/no-important'
 
 import PolyhedronTable from './PolyhedronTable'
 import { hoeflerText, andaleMono } from '../styles/fonts'
+import periodicTable from '../constants/periodicTable'
 
-const platonicArchimedean = {
-  caption: 'Platonic and Archimedean Solids',
-  rows: [
-    'regular',
-    'truncated',
-    'rectified',
-    'cantellated',
-    'bevelled',
-    'snub',
-  ],
-  columns: [
-    { name: '', sub: ['tetrahedron'] },
-    { name: '', sub: ['cube', 'octahedron'] },
-    { name: '', sub: ['dodecahedron', 'icosahedron'] },
-  ],
-  data: [
-    ['T', ['C', 'O'], ['D', 'I']],
-    ['tT', ['tC', 'tO'], ['tD', 'tI']],
-    ['!O', 'aC', 'aD'],
-    ['!aC', 'eC', 'eD'],
-    ['!tO', 'bC', 'bD'],
-    ['!I', 'sC', 'sD'],
-  ],
-}
-
-const prisms = {
-  caption: 'Prisms and Antiprisms',
-  rows: [
-    'triangular',
-    'square',
-    'pentagonal',
-    'hexagonal',
-    'octagonal',
-    'decagonal',
-  ],
-  columns: ['prism', 'antiprism'],
-  data: _.zip(
-    ['P3', '!C', 'P5', 'P6', 'P8', 'P10'],
-    ['!O', 'A4', 'A5', 'A6', 'A8', 'A10'],
-  ),
-}
-
-const pyramidsCupolae = {
-  caption: 'Pyramids, Cupoplæ, and Rotundæ',
-  rows: [
-    'triangular pyramid',
-    'square pyramid',
-    'pentagonal pyramid',
-    'digonal cupola',
-    'triangular cupola',
-    'square cupola',
-    'pentagonal cupola',
-    'cupola-rotunda',
-    'pentagonal rotunda',
-  ],
-  columns: [
-    '',
-    'elongated',
-    'gyroelongated',
-    { name: 'bi-', sub: ['ortho-', 'gyro-'] },
-    { name: 'elongated bi-', sub: ['ortho-', 'gyro-'] },
-    'gyroelongated bi-',
-  ],
-  data: _.zip(
-    ['!T', 'J1', 'J2', '!P3', 'J3', 'J4', 'J5', '', 'J6'],
-    ['J7', 'J8', 'J9', 'coplanar', 'J18', 'J19', 'J20', '', 'J21'],
-    ['coplanar', 'J10', 'J11', 'concave', 'J22', 'J23', 'J24', '', 'J25'],
-    [
-      'J12',
-      '!O',
-      'J13',
-      ['coplanar', 'J26'],
-      ['J27', '!aC'],
-      ['J28', 'J29'],
-      ['J30', 'J31'],
-      ['J32', 'J33'],
-      ['J34', '!aD'],
-    ],
-    [
-      'J14',
-      'J15',
-      'J16',
-      ['coplanar', 'coplanar'],
-      ['J35', 'J36'],
-      ['!eC', 'J37'],
-      ['J38', 'J39'],
-      ['J40', 'J41'],
-      ['J42', 'J43'],
-    ],
-    ['coplanar', 'J17', '!I', 'concave', 'J44', 'J45', 'J46', 'J47', 'J48'],
-  ),
-}
-
-const augmentedSolids = {
-  caption: 'Augmented Polyhedra',
-  rows: [
-    'triangular prism',
-    'pentagonal prism',
-    'hexagonal prism',
-    'dodecahedron',
-    'truncated tetrahedron',
-    'truncated cube',
-    'truncated dodecahedron',
-  ],
-  columns: [
-    'augmented',
-    { name: 'biaugmented', sub: ['para-', 'meta-'] },
-    'triaugmented',
-  ],
-  data: [
-    ['J49', 'J50', 'J51'],
-    ['J52', 'J53'],
-    ['J54', ['J55', 'J56'], 'J57'],
-    ['J58', ['J59', 'J60'], 'J61'],
-    ['J65'],
-    ['J66', 'J67'],
-    ['J68', ['J69', 'J70'], 'J71'],
-  ],
-}
-
-const diminishedIcosahedra = {
-  caption: 'Diminished Icosahedra',
-  rows: ['icosahedron'],
-  columns: [
-    'tridiminished',
-    { name: 'bidiminished', sub: ['para-', 'meta-'] },
-    'diminished',
-    'augmented tridiminished',
-  ],
-  data: [['J63', ['!A5', 'J62'], '!J11', 'J64']],
-}
-
-const rhombicosidodecahedra = {
-  caption: 'Gyrate and Diminished Rhombicosidodecahedra',
-  rows: [
-    '',
-    'diminished',
-    'parabidiminished',
-    'metabidiminished',
-    'tridiminished',
-  ],
-  columns: [
-    '',
-    { name: 'gyrate', sub: ['para-', 'meta-'] },
-    { name: 'bigyrate', sub: ['para-', 'meta-'] },
-    'trigyrate',
-  ],
-  data: [
-    ['!eD', 'J72', ['J73', 'J74'], 'J75'],
-    ['J76', ['J77', 'J78'], 'J79'],
-    ['J80'],
-    ['J81', 'J82'],
-    ['J83'],
-  ],
-}
-
-const snubAntiprisms = {
-  caption: 'Snub Antiprisms',
-  rows: ['snub'],
-  columns: ['disphenoid', 'triangular', 'square antiprism'],
-  data: [['J84', '!I', 'J85']],
-}
-
-const otherJohnson = {
-  caption: 'Other Johnson Solids',
-  rows: [''],
-  columns: ['', '', '', '', '', '', ''],
-  data: [['J86', 'J87', 'J88', 'J89', 'J90', 'J91', 'J92']],
+const gridAreaMapping = {
+  'Platonic and Archimedean Solids': 'plato',
+  'Prisms and Antiprisms': 'prism',
+  'Pyramids, Cupoplæ, and Rotundæ': 'pyrCup',
+  'Augmented Polyhedra': 'aug',
+  'Diminished Icosahedra': 'icos',
+  'Gyrate and Diminished Rhombicosidodecahedra': 'rhombicos',
+  'Snub Antiprisms': 'snub',
+  'Other Johnson Solids': 'other',
+  'Johnson Solids': 'J',
 }
 
 const styles = StyleSheet.create({
@@ -219,9 +62,10 @@ const styles = StyleSheet.create({
 
   subheader: {
     marginLeft: 50,
+    fontFamily: hoeflerText,
     transform: 'rotate(-90deg)',
     overflow: 'hidden',
-    fontFamily: hoeflerText,
+    whiteSpace: 'nowrap',
   },
 
   wikiLink: {
@@ -287,17 +131,17 @@ export default function PeriodicTable() {
             (e.g. a Platonic solid in the prism section) it is grayed out.
           </p>
         </GridArea>
-        <PolyhedronTableArea area="plato" data={platonicArchimedean} />
-        <PolyhedronTableArea area="prism" data={prisms} />
-        <GridArea area="J" element="h2" classes={styles.subheader}>
-          Johnson&nbsp;Solids
-        </GridArea>
-        <PolyhedronTableArea area="pyrCup" data={pyramidsCupolae} />
-        <PolyhedronTableArea area="aug" data={augmentedSolids} />
-        <PolyhedronTableArea area="icos" data={diminishedIcosahedra} />
-        <PolyhedronTableArea area="rhombicos" data={rhombicosidodecahedra} />
-        <PolyhedronTableArea area="snub" data={snubAntiprisms} />
-        <PolyhedronTableArea area="other" data={otherJohnson} />
+        {periodicTable.map(section => {
+          const area = gridAreaMapping[section.caption]
+          if (section.type === 'subheader') {
+            return (
+              <GridArea area={area} element="h2" classes={styles.subheader}>
+                {section.caption}
+              </GridArea>
+            )
+          }
+          return <PolyhedronTableArea area={area} data={section} />
+        })}
       </div>
     </main>
   )
---
+++ src/constants/periodicTable.js
@@ -0,0 +1,178 @@
+import * as _ from 'lodash'
+
+export default [
+  {
+    caption: 'Platonic and Archimedean Solids',
+    rows: [
+      'regular',
+      'truncated',
+      'rectified',
+      'cantellated',
+      'bevelled',
+      'snub',
+    ],
+    columns: [
+      { name: '', sub: ['tetrahedron'] },
+      { name: '', sub: ['cube', 'octahedron'] },
+      { name: '', sub: ['dodecahedron', 'icosahedron'] },
+    ],
+    data: [
+      ['T', ['C', 'O'], ['D', 'I']],
+      ['tT', ['tC', 'tO'], ['tD', 'tI']],
+      ['!O', 'aC', 'aD'],
+      ['!aC', 'eC', 'eD'],
+      ['!tO', 'bC', 'bD'],
+      ['!I', 'sC', 'sD'],
+    ],
+  },
+
+  {
+    caption: 'Prisms and Antiprisms',
+    rows: [
+      'triangular',
+      'square',
+      'pentagonal',
+      'hexagonal',
+      'octagonal',
+      'decagonal',
+    ],
+    columns: ['prism', 'antiprism'],
+    data: _.zip(
+      ['P3', '!C', 'P5', 'P6', 'P8', 'P10'],
+      ['!O', 'A4', 'A5', 'A6', 'A8', 'A10'],
+    ),
+  },
+
+  {
+    caption: 'Johnson Solids',
+    type: 'subheader',
+  },
+
+  {
+    caption: 'Pyramids, Cupoplæ, and Rotundæ',
+    rows: [
+      'triangular pyramid',
+      'square pyramid',
+      'pentagonal pyramid',
+      'digonal cupola',
+      'triangular cupola',
+      'square cupola',
+      'pentagonal cupola',
+      'cupola-rotunda',
+      'pentagonal rotunda',
+    ],
+    columns: [
+      '',
+      'elongated',
+      'gyroelongated',
+      { name: 'bi-', sub: ['ortho-', 'gyro-'] },
+      { name: 'elongated bi-', sub: ['ortho-', 'gyro-'] },
+      'gyroelongated bi-',
+    ],
+    data: _.zip(
+      ['!T', 'J1', 'J2', '!P3', 'J3', 'J4', 'J5', '', 'J6'],
+      ['J7', 'J8', 'J9', 'coplanar', 'J18', 'J19', 'J20', '', 'J21'],
+      ['coplanar', 'J10', 'J11', 'concave', 'J22', 'J23', 'J24', '', 'J25'],
+      [
+        'J12',
+        '!O',
+        'J13',
+        ['coplanar', 'J26'],
+        ['J27', '!aC'],
+        ['J28', 'J29'],
+        ['J30', 'J31'],
+        ['J32', 'J33'],
+        ['J34', '!aD'],
+      ],
+      [
+        'J14',
+        'J15',
+        'J16',
+        ['coplanar', 'coplanar'],
+        ['J35', 'J36'],
+        ['!eC', 'J37'],
+        ['J38', 'J39'],
+        ['J40', 'J41'],
+        ['J42', 'J43'],
+      ],
+      ['coplanar', 'J17', '!I', 'concave', 'J44', 'J45', 'J46', 'J47', 'J48'],
+    ),
+  },
+
+  {
+    caption: 'Augmented Polyhedra',
+    rows: [
+      'triangular prism',
+      'pentagonal prism',
+      'hexagonal prism',
+      'dodecahedron',
+      'truncated tetrahedron',
+      'truncated cube',
+      'truncated dodecahedron',
+    ],
+    columns: [
+      'augmented',
+      { name: 'biaugmented', sub: ['para-', 'meta-'] },
+      'triaugmented',
+    ],
+    data: [
+      ['J49', 'J50', 'J51'],
+      ['J52', 'J53'],
+      ['J54', ['J55', 'J56'], 'J57'],
+      ['J58', ['J59', 'J60'], 'J61'],
+      ['J65'],
+      ['J66', 'J67'],
+      ['J68', ['J69', 'J70'], 'J71'],
+    ],
+  },
+
+  {
+    caption: 'Diminished Icosahedra',
+    rows: ['icosahedron'],
+    columns: [
+      'tridiminished',
+      { name: 'bidiminished', sub: ['para-', 'meta-'] },
+      'diminished',
+      'augmented tridiminished',
+    ],
+    data: [['J63', ['!A5', 'J62'], '!J11', 'J64']],
+  },
+
+  {
+    caption: 'Gyrate and Diminished Rhombicosidodecahedra',
+    rows: [
+      '',
+      'diminished',
+      'parabidiminished',
+      'metabidiminished',
+      'tridiminished',
+    ],
+    columns: [
+      '',
+      { name: 'gyrate', sub: ['para-', 'meta-'] },
+      { name: 'bigyrate', sub: ['para-', 'meta-'] },
+      'trigyrate',
+    ],
+    data: [
+      ['!eD', 'J72', ['J73', 'J74'], 'J75'],
+      ['J76', ['J77', 'J78'], 'J79'],
+      ['J80'],
+      ['J81', 'J82'],
+      ['J83'],
+    ],
+  },
+
+  {
+    caption: 'Snub Antiprisms',
+    rows: ['snub'],
+    columns: ['disphenoid', 'triangular', 'square antiprism'],
+    data: [['J84', '!I', 'J85']],
+  },
+
+  {
+    caption: 'Other Johnson Solids',
+    rows: [''],
+    columns: ['', '', '', '', '', '', ''],
+    data: [['J86', 'J87', 'J88', 'J89', 'J90', 'J91', 'J92']],
+  },
+]
