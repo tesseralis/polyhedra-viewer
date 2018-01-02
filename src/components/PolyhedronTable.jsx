0861b4252d6745fb6b6b5068c828e923ec00d4dc
Nat Alison <tesseralis@gmail.com>
2018-01-01 21:26:49 -0800

    refactors

--- src/components/PolyhedronTable.jsx
+++ src/components/PolyhedronTable.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import * as _ from 'lodash'
 import { css, StyleSheet } from 'aphrodite'
 
-import johnsonNames from '../data/groups/johnson.json'
 import PolyhedronLink from './PolyhedronLink'
 import { hoeflerText } from '../styles/fonts'
+import { fromConwayNotation } from '../constants/polyhedra'
 
 const styles = StyleSheet.create({
   table: {
@@ -25,65 +25,11 @@ const styles = StyleSheet.create({
   },
 })
 
-// FIXME I think these are duplicated to a certain extent from the data.
-// Move these over to the "data" section
-const prismNames = {
-  3: 'triangular',
-  4: 'square',
-  5: 'pentagonal',
-  6: 'hexagonal',
-  8: 'octagonal',
-  10: 'decagonal',
-}
-
-const platonicMapping = {
-  T: 'tetrahedron',
-  C: 'cube',
-  O: 'octahedron',
-  D: 'dodecahedron',
-  I: 'icosahedron',
-}
-
-const archimedeanMapping = {
-  tT: 'truncated tetrahedron',
-  aC: 'cuboctahedron',
-  tC: 'truncated cube',
-  tO: 'truncated octahedron',
-  eC: 'rhombicuboctahedron',
-  bC: 'truncated cuboctahedron',
-  sC: 'snub cube',
-  aD: 'icosidodecahedron',
-  tD: 'truncated dodecahedron',
-  tI: 'truncated icosahedron',
-  eD: 'rhombicosidodecahedron',
-  bD: 'truncated icosidodecahedron',
-  sD: 'snub dodecahedron',
-}
-
-const getPolyhedronFor = notation => {
-  const prefix = notation[0]
-  const number = notation.substring(1)
-  if (platonicMapping[notation]) {
-    return platonicMapping[notation]
-  }
-  if (archimedeanMapping[notation]) {
-    return archimedeanMapping[notation]
-  }
-  if (prefix === 'J') {
-    return johnsonNames[number - 1]
-  }
-  if (prefix === 'P') {
-    return `${prismNames[number]} prism`
-  }
-  if (prefix === 'A') {
-    return `${prismNames[number]} antiprism`
-  }
-  return null
-}
-
 const Cell = ({ cell, colSpan = 1 }) => {
   const isFake = cell[0] === '!'
-  const polyhedron = getPolyhedronFor(isFake ? cell.substring(1) : cell)
+  const polyhedron = fromConwayNotation(isFake ? cell.substring(1) : cell)
+
+  // Render a link for each cell, or a grayed-out link when indicated by an "!"
   return (
     <td className={css(styles.cell)} colSpan={colSpan}>
       {polyhedron ? <PolyhedronLink isFake={isFake} name={polyhedron} /> : cell}
@@ -91,51 +37,66 @@ const Cell = ({ cell, colSpan = 1 }) => {
   )
 }
 
+// Return whether this column has subcolumns
+// (Right now, we check if it's a string or an object)
+const hasSubColumn = column => !_.isString(column)
+
+const ColumnHeaders = ({ columns }) => {
+  return (
+    <thead>
+      {/* Render the subcolumn headers first, where they exist. */}
+      <tr>
+        <th />
+        {_.flatMap(
+          columns,
+          (col, j) =>
+            !hasSubColumn(col) ? (
+              <th key={j} />
+            ) : (
+              col.sub.map(subCol => (
+                <th className={css(styles.cell)} key={`${j}-${subCol}`}>
+                  {subCol}
+                </th>
+              ))
+            ),
+        )}
+      </tr>
+      {/* Render the main column headers, making sure to span more than one column for those with subcolumns */}
+      <tr>
+        <th />
+        {columns.map((col, j) => (
+          <th
+            className={css(styles.cell)}
+            key={j}
+            colSpan={hasSubColumn(col) ? col.sub.length : 1}
+          >
+            {hasSubColumn(col) ? col.name : col}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  )
+}
+
 export default function PolyhedronTable({ caption, rows, columns, data }) {
   return (
     <table className={css(styles.table)}>
       <caption className={css(styles.caption)}>{caption}</caption>
-      <thead>
-        <tr>
-          <th />
-          {_.flatMap(
-            columns,
-            (col, j) =>
-              _.isString(col) ? (
-                <th key={j} />
-              ) : (
-                col.sub.map(subCol => (
-                  <th className={css(styles.cell)} key={`${j}-${subCol}`}>
-                    {subCol}
-                  </th>
-                ))
-              ),
-          )}
-        </tr>
-        <tr>
-          <th />
-          {columns.map((col, j) => (
-            <th
-              className={css(styles.cell)}
-              key={j}
-              colSpan={_.isString(col) ? 1 : col.sub.length}
-            >
-              {_.isString(col) ? col : col.name}
-            </th>
-          ))}
-        </tr>
-      </thead>
+      <ColumnHeaders columns={columns} />
       <tbody>
         {data.map((row, i) => (
           <tr key={i}>
+            {/* Row header */}
             <th className={css(styles.cell)}>{rows[i]}</th>
             {_.flatMap(row, (cell, j) => {
               const col = columns[j]
-              if (_.isString(col)) {
+              if (!hasSubColumn(col)) {
                 return <Cell key={j} cell={cell} />
               } else if (!_.isArray(cell)) {
+                // If the cell does *not* have subcells, make it span the length of the subcolumns
                 return <Cell key={j} cell={cell} colSpan={col.sub.length} />
               } else {
+                // If the cell does have subcells render and return them
                 return cell.map((subcell, k) => (
                   <Cell key={`${j}-${k}`} cell={subcell} />
                 ))
--- src/constants/polyhedra.js
+++ src/constants/polyhedra.js
@@ -5,11 +5,12 @@ const getPolyhedra = groupName => require(`../data/groups/${groupName}.json`)
 /* Johnson Solid Subgroups */
 const johnsonSolids = getPolyhedra('johnson')
 const johnsonSubgroups = require('../data/johnsonSubgroups.json')
-const getEndIndex = i => i === johnsonSubgroups.length - 1 ? 92 : johnsonSubgroups[i+1].index
+const getEndIndex = i =>
+  i === johnsonSubgroups.length - 1 ? 92 : johnsonSubgroups[i + 1].index
 const getJohnsonPolyhedra = () => {
   return johnsonSubgroups.map(({ name, index }, i) => ({
     name,
-    polyhedra: johnsonSolids.slice(index, getEndIndex(i))
+    polyhedra: johnsonSolids.slice(index, getEndIndex(i)),
   }))
 }
 
@@ -40,4 +41,58 @@ export const getSolidData = escapedSolidName => {
   return require(`../data/polyhedra/${escapedSolidName}.json`)
 }
 
-export const escapeName = name => name.replace(/ /g, '-');
+export const escapeName = name => name.replace(/ /g, '-')
+
+const prismNames = {
+  3: 'triangular',
+  4: 'square',
+  5: 'pentagonal',
+  6: 'hexagonal',
+  8: 'octagonal',
+  10: 'decagonal',
+}
+
+const platonicMapping = {
+  T: 'tetrahedron',
+  C: 'cube',
+  O: 'octahedron',
+  D: 'dodecahedron',
+  I: 'icosahedron',
+}
+
+const archimedeanMapping = {
+  tT: 'truncated tetrahedron',
+  aC: 'cuboctahedron',
+  tC: 'truncated cube',
+  tO: 'truncated octahedron',
+  eC: 'rhombicuboctahedron',
+  bC: 'truncated cuboctahedron',
+  sC: 'snub cube',
+  aD: 'icosidodecahedron',
+  tD: 'truncated dodecahedron',
+  tI: 'truncated icosahedron',
+  eD: 'rhombicosidodecahedron',
+  bD: 'truncated icosidodecahedron',
+  sD: 'snub dodecahedron',
+}
+
+export const fromConwayNotation = notation => {
+  const prefix = notation[0]
+  const number = notation.substring(1)
+  if (platonicMapping[notation]) {
+    return platonicMapping[notation]
+  }
+  if (archimedeanMapping[notation]) {
+    return archimedeanMapping[notation]
+  }
+  if (prefix === 'J') {
+    return johnsonSolids[number - 1]
+  }
+  if (prefix === 'P') {
+    return `${prismNames[number]} prism`
+  }
+  if (prefix === 'A') {
+    return `${prismNames[number]} antiprism`
+  }
+  return null
+}
