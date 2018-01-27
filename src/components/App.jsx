588f372fcfb2fb1e3838fbd66c6ab70cc2a6b4e4
Nat Alison <tesseralis@gmail.com>
2018-01-26 20:10:43 -0800

    move browser router to the root

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import * as queryString from 'query-string'
 
 import 'styles/reset.css'
@@ -8,16 +8,13 @@ import 'styles/box-sizing.css'
 import { PeriodicTable } from './table'
 import { Viewer } from './viewer'
 
-const ViewerComponent = ({ match, location }) => {
-  const parsed = queryString.parse(location.search)
-  return <Viewer solid={match.params.solid} operation={parsed.op} />
+const ViewerComponent = ({ match }) => {
+  return <Viewer solid={match.params.solid} />
 }
 
 export default () => (
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={PeriodicTable} />
-      <Route path="/:solid" component={ViewerComponent} />
-    </div>
-  </BrowserRouter>
+  <div>
+    <Route exact path="/" component={PeriodicTable} />
+    <Route path="/:solid" component={ViewerComponent} />
+  </div>
 )
--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 import App from './App'
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>
 )
 
