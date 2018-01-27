f25395bd0d3a859c8e898984173d37a88f6cd108
Nat Alison <tesseralis@gmail.com>
2018-01-26 20:18:55 -0800

    Right lodash import

--- src/constants/relations.js
+++ src/constants/relations.js
@@ -1,4 +1,4 @@
-import * as _ from 'lodash'
+import _ from 'lodash'
 import { fromConwayNotation, toConwayNotation } from 'constants/polyhedra'
 import periodicTable from 'constants/periodicTable'
 
