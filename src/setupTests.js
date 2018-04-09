d4486cd96a12397581f56d31b84d5c30c9e445f1
Nat Alison <tesseralis@gmail.com>
2018-04-09 16:36:13 -0700

    quick fix for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+// import Adapter from 'enzyme-adapter-react-16'
 import { StyleSheetTestUtils } from 'aphrodite'
+import Adapter from './ReactSixteenAdapter'
 
 configure({ adapter: new Adapter() })
 StyleSheetTestUtils.suppressStyleInjection()
