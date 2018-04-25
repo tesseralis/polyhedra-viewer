3e28bedf2fba48d93f79c663982c509ef5b5ce68
Nat Alison <tesseralis@gmail.com>
2018-04-24 20:09:51 -0700

    add flow libraries

--- src/math/linAlg.js
+++ src/math/linAlg.js
@@ -1,16 +1,19 @@
+// @flow
 import _ from 'lodash';
 import { geom } from 'toxiclibsjs';
 const { Vec3D, Triangle3D, Plane } = geom;
+
+export type Vector = number[];
 
 export const PRECISION_DIGITS = 3;
 export const PRECISION = Math.pow(10, -PRECISION_DIGITS);
 
 // convert an array of vertices into a vector
-export const vec = p => new Vec3D(...p);
+export const vec = (p: Vector) => new Vec3D(...p);
 
-export const getMidpoint = (v1, v2) => v1.add(v2).scale(0.5);
+export const getMidpoint = (v1: Vec3D, v2: Vec3D) => v1.add(v2).scale(0.5);
 // Get the plane containing the given points
-export function getPlane(points) {
+export function getPlane(points: Vec3D[]) {
   if (points.length < 3) {
     throw new Error('Need at least three points for a plane');
   }
@@ -19,17 +22,17 @@ export function getPlane(points) {
 }
 
 // Return whether the set of points lie on a plane
-export function isPlanar(points) {
+export function isPlanar(points: Vec3D[]) {
   const plane = getPlane(points);
   return _.every(points, vec => plane.getDistanceToPoint(vec) < PRECISION);
 }
 
-export function getCentroid(vectors) {
+export function getCentroid(vectors: Vec3D[]) {
   return vectors.reduce((v1, v2) => v1.add(v2)).scale(1 / vectors.length);
 }
 
 // Get the normal of a polygon given its ordered vertices
-export function getNormal(vertices) {
+export function getNormal(vertices: Vec3D[]) {
   const [v0, v1, v2] = vertices;
   return v0.sub(v1).cross(v1.sub(v2));
 }
--- src/math/solidUtils.js
+++ src/math/solidUtils.js
@@ -1,25 +1,34 @@
+// @flow
+
 import _ from 'lodash';
+import type { Vector } from './linAlg';
+
+export type Vertex = [Vector, Vector, Vector];
+export type VertexIndex = number;
+
+export type Face = VertexIndex[];
+export type FaceIndex = number;
 
 // Get the element of the array at the given index,
 // modulo its length
 function mod(a, b) {
   return a >= 0 ? a % b : a % b + b;
 }
-export function getCyclic(array, index) {
+export function getCyclic(array: number[], index: number) {
   return array[mod(index, array.length)];
 }
 
 // Return the number of sides of a face
-export const numSides = face => face.length;
+export const numSides = (face: Face) => face.length;
 
-export function getDirectedEdges(face) {
+export function getDirectedEdges(face: Face) {
   return _.map(face, (vertex, index) => {
     return [vertex, getCyclic(face, index + 1)];
   });
 }
 
 // Get a "face" (list of vertices) representing the boundary of the given faces
-export function getBoundary(faces) {
+export function getBoundary(faces: Face[]) {
   const edges = {};
   // build up a lookup table for every pair of edges to that face
   _.forEach(faces, (face, index) => {
@@ -49,10 +58,10 @@ export function getBoundary(faces) {
   return result;
 }
 
-export function nextVertex(face, vIndex) {
+export function nextVertex(face: Face, vIndex: VertexIndex) {
   return getCyclic(face, face.indexOf(vIndex) + 1);
 }
 
-export function prevVertex(face, vIndex) {
+export function prevVertex(face: Face, vIndex: VertexIndex) {
   return getCyclic(face, face.indexOf(vIndex) - 1);
 }
