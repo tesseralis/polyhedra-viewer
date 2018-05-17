447048c829e2f3225a25193bf595bcdcc392a8a1
Nat Alison <tesseralis@gmail.com>
2018-05-17 04:10:12 -0700

    typed icon

--- src/components/viewer/sidebar/Icon.jsx
+++ src/components/viewer/sidebar/Icon.jsx
@@ -1,8 +1,14 @@
+// @flow strict
 import React from 'react';
 import _ from 'lodash';
 import 'mdi/css/materialdesignicons.min.css';
 
-const Icon = ({ name, size }) => {
+interface Props {
+  name: string;
+  size?: 18 | 24 | 36 | 48;
+}
+
+const Icon = ({ name, size }: Props) => {
   const classes = _.compact(['mdi', `mdi-${name}`, size && `mdi-${size}px`]);
 
   return <i className={classes.join(' ')} aria-hidden="true" />;
