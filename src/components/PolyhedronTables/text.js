45297cb6b1095944055369f570f44ddbb564e0b6
Nat Alison <tesseralis@gmail.com>
2018-06-20 18:24:09 -0700

    holy fuck this cafe is obnoxious

--- src/components/PolyhedronTables/text.js
+++ src/components/PolyhedronTables/text.js
@@ -6,7 +6,8 @@ export const abstract = `
   polyhedra. These include the five [Platonic solids][platonic], the 13
   [Archimedean solids][archimedean], the infinite set of [prisms][prism]
   and [antiprisms][antiprism], and the 92 [Johnson solids][johnson].
-  Select a solid to play around with it and to see its
+  The 120 solids presented here are connected to each other by an intricate
+  network of operations. Select a solid to play around with it and to see its
   relationships with other polyhedra.
 
   [platonic]: http://en.wikipedia.org/wiki/Platonic_solid
