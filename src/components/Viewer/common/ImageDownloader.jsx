05be7f422e094eb3bd6c17c5514cf983a25ba2f0
Nat Alison <tesseralis@gmail.com>
2018-07-02 14:38:05 -0700

    promises

--- src/components/Viewer/common/ImageDownloader.jsx
+++ src/components/Viewer/common/ImageDownloader.jsx
@@ -2,25 +2,43 @@
 import * as React from 'react';
 import JSZip from 'jszip';
 import FileSaver from 'file-saver';
-import { WithPolyhedron } from '../context';
+
+import { escapeName } from 'polyhedra/names';
 import { allSolidNames } from 'data';
+import { WithPolyhedron } from '../context';
 
 class ImageDownloader extends React.Component<*> {
   downloadImages = () => {
     const zip = new JSZip();
-    const { solid, setPolyhedron } = this.props;
     const canvas = document.getElementsByTagName('canvas');
+    const ratio = canvas[0].width / canvas[0].height;
+    canvas[0].height = 200;
+    canvas[0].width = ratio * 200;
 
-    // allSolidNames.forEach(solid => {
-    //   setPolyhedron(solid);
-    // });
+    const images = zip.folder('images');
+    const tasks = allSolidNames.map(solid => () =>
+      this.addImage(canvas, images, solid),
+    );
 
-    canvas[0].toBlob(v => {
-      const images = zip.folder('images');
-      images.file(`${solid}.png`, v, { base64: true });
-
+    let result = Promise.resolve();
+    tasks.forEach(task => {
+      result = result.then(() => task());
+    });
+    result.then(() => {
       zip.generateAsync({ type: 'blob' }).then(content => {
         FileSaver.saveAs(content, 'images.zip');
+      });
+    });
+  };
+
+  addImage = (canvas, folder, solid) => {
+    const { setPolyhedron } = this.props;
+    return new Promise(resolve => {
+      setPolyhedron(solid, () => {
+        canvas[0].toBlob(v => {
+          folder.file(`${escapeName(solid)}.png`, v, { base64: true });
+          resolve();
+        });
       });
     });
   };
