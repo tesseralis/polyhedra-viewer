908d0fbf95c5f53687a9cbdabe7927e66ce822d2
Nat Alison <tesseralis@gmail.com>
2018-07-27 17:10:19 -0700

    move react-ga code to index.js

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,16 +1,10 @@
 // @flow
 import React from 'react';
-import ReactGA from 'react-ga';
 import { BrowserRouter } from 'react-router-dom';
 import { DeviceProvider } from './DeviceContext';
 import { ConfigProvider } from './ConfigContext';
 
 import App from './App';
-
-if (process.env.NODE_ENV === 'production') {
-  ReactGA.initialize('UA-122991684-1');
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
 
 const Root = () => (
   <BrowserRouter>
