0b1b323e37c500fe10721f1375a2668b077bd746
Nat Alison <tesseralis@gmail.com>
2018-10-10 16:36:58 -0700

    update react and enzyme

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,6 @@
 // @flow
 import { configure } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-16';
-import Adapter from './ReactSixteenAdapter';
+import Adapter from 'enzyme-adapter-react-16';
 import { StyleSheetTestUtils } from 'aphrodite';
 
 configure({ adapter: new Adapter() });
