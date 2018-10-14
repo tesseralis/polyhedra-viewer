b5ebda8bc5125a56ddfcfa6c8652984b51e901ca
Nat Alison <tesseralis@gmail.com>
2018-10-13 19:05:22 -0700

    add link to polyhedronisme

--- src/components/HomePage/text.js
+++ src/components/HomePage/text.js
@@ -127,6 +127,7 @@ check out these links:
 * [Virtual Polyhedra] by George W. Hart - an extensive encyclopedia of polyhedra and the major
   inspiration for this site
 * [Visual Polyhedra] by David I. McCooey - More polyhedral models with extensive geometric data
+* [polyHédronisme] by Anselm Levskaya - Build complex polyhedra using Conway operations
 * [Polyhedra] by Stacy Speyer - Paper models of polyhedra and other artistic imaginings
 * [Johnson Solids] by Allison Chen - Diagrams categorizing the Johnson solids based on their operations
 * [4D Toys] - An interactive catalogue of 4D shapes
@@ -134,6 +135,7 @@ check out these links:
 
 [Virtual Polyhedra]: http://www.georgehart.com/virtual-polyhedra/vp.html
 [Visual Polyhedra]: http://dmccooey.com/polyhedra/
+[polyHédronisme]: http://levskaya.github.io/polyhedronisme/
 [Polyhedra]: http://polyhedra.stacyspeyer.net/
 [Johnson Solids]: http://portfolios.risd.edu/gallery/Johnson-Solids/8807383
 [4D Toys]: http://4dtoys.com/
