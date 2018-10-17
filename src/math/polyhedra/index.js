209aabd1fe736284b773fa1583dde55fea5e1588
Nat Alison <tesseralis@gmail.com>
2018-10-16 21:26:52 -0700

    upgrade react-scripts

--- src/math/polyhedra/index.js
+++ src/math/polyhedra/index.js
@@ -1,11 +1,13 @@
 // @flow strict
 
 export { default as Polyhedron } from './Polyhedron';
-export { default as Vertex, VertexList } from './Vertex';
+export { default as Vertex } from './Vertex';
+export type { VertexList } from './Vertex';
 export { default as Face } from './Face';
 export { default as Edge } from './Edge';
 export { default as Cap } from './Cap';
 
 export * from './solidTypes';
 
-export { VertexArg, normalizeVertex } from './SolidBuilder';
+export { normalizeVertex } from './SolidBuilder';
+export type { VertexArg } from './SolidBuilder';
