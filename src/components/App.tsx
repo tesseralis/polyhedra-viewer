3a96f74907fc10db4b3a9290277c4b9e37628355
Nat Alison <tesseralis@gmail.com>
2020-01-29 22:59:22 -0800

    use suspense in favor of react loadable

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
-import Loadable from 'react-loadable';
 
 import { isValidSolid } from 'data';
 import {
@@ -15,47 +14,44 @@ import {
 import ErrorPage from './ErrorPage';
 import Loading from './Loading';
 
-const HomePage = Loadable({
-  loader: () => import('./HomePage'),
-  loading: Loading,
-});
-
-const Viewer = Loadable({
-  loader: () => import('./Viewer'),
-  loading: Loading,
-});
+const HomePage = React.lazy(() => import('./HomePage'));
+const Viewer = React.lazy(() => import('./Viewer'));
 
 export default () => (
-  <Switch>
-    <Route
-      exact
-      path="/"
-      render={({ location }) => <HomePage hash={location.hash.substring(1)} />}
-    />
-    <Route
-      exact
-      path="/random"
-      render={() => <Redirect to={randomSolidName()} />}
-    />
-    <Route
-      path="/:solid"
-      render={({ match, history }) => {
-        const solid = match.params.solid || '';
-        if (isConwaySymbol(solid)) {
-          const fullName = escapeName(fromConwayNotation(solid));
-          const newPath = history.location.pathname.replace(solid, fullName);
-          return <Redirect to={newPath} />;
-        }
-        if (isAlternateName(solid)) {
-          const fullName = escapeName(getCanonicalName(solid));
-          const newPath = history.location.pathname.replace(solid, fullName);
-          return <Redirect to={newPath} />;
-        }
-        if (isValidSolid(solid)) {
-          return <Viewer solid={solid} url={match.url} history={history} />;
-        }
-        return <ErrorPage />;
-      }}
-    />
-  </Switch>
+  <Suspense fallback={<Loading />}>
+    <Switch>
+      <Route
+        exact
+        path="/"
+        render={({ location }) => (
+          <HomePage hash={location.hash.substring(1)} />
+        )}
+      />
+      <Route
+        exact
+        path="/random"
+        render={() => <Redirect to={randomSolidName()} />}
+      />
+      <Route
+        path="/:solid"
+        render={({ match, history }) => {
+          const solid = match.params.solid || '';
+          if (isConwaySymbol(solid)) {
+            const fullName = escapeName(fromConwayNotation(solid));
+            const newPath = history.location.pathname.replace(solid, fullName);
+            return <Redirect to={newPath} />;
+          }
+          if (isAlternateName(solid)) {
+            const fullName = escapeName(getCanonicalName(solid));
+            const newPath = history.location.pathname.replace(solid, fullName);
+            return <Redirect to={newPath} />;
+          }
+          if (isValidSolid(solid)) {
+            return <Viewer solid={solid} url={match.url} history={history} />;
+          }
+          return <ErrorPage />;
+        }}
+      />
+    </Switch>
+  </Suspense>
 );
