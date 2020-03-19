5697b806efdf2fb6bca32068ca4cfb47e85ceb61
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-03-18 23:18:42 -0700

    refactor getSymmetry to use a visitor

--- src/data/symmetry/getSymmetry.ts
+++ src/data/symmetry/getSymmetry.ts
@@ -1,13 +1,4 @@
-import {
-  classicals,
-  prisms,
-  capstones,
-  augmentedPrisms,
-  augmentedClassicals,
-  diminishedIcosahedra,
-  rhombicosidodecahedra,
-  snubAntiprisms,
-} from "../tables"
+import visitTables from "../tables/visitTables"
 import { assertValidSolid } from "../common"
 import { Family, Symmetry, Polyhedral, Cyclic, Dihedral } from "./Symmetry"
 
@@ -15,129 +6,6 @@ const groupNames: Record<3 | 4 | 5, Family> = {
   3: "tetrahedral",
   4: "octahedral",
   5: "icosahedral",
-}
-
-function getClassicalSymmetry(name: string) {
-  const { family, operation } = classicals.get(name)
-  return Polyhedral.get(groupNames[family], operation === "snub")
-}
-
-function getPrismSymmetry(name: string) {
-  const { base, type } = prisms.get(name)!
-  return Dihedral.get(base, type)
-}
-
-function getCapstoneSymmetry(name: string) {
-  const { base, type, count, elongation, gyrate } = capstones.get(name)
-  // Single-count capstones all have pyramidal symmetry
-  if (count === 1) {
-    return Cyclic.get(base)
-  }
-  const gyroelongated = elongation === "antiprism"
-
-  if (type === "pyramid") {
-    return Dihedral.get(base, gyroelongated ? "antiprism" : "prism")
-  } else if (type === "cupolarotunda") {
-    return Cyclic.get(base, !!gyroelongated)
-  } else {
-    // cupolae and rotundae
-    const reflection = gyroelongated
-      ? undefined
-      : gyrate === "gyro"
-      ? "antiprism"
-      : "prism"
-    return Dihedral.get(base, reflection)
-  }
-}
-
-function getAugmentedPrismSymmetry(name: string) {
-  const { count, align } = augmentedPrisms.get(name)
-  switch (count) {
-    case 1: {
-      // mono-augmented prisms all have biradial symmetry
-      return Cyclic.biradial
-    }
-    case 2: {
-      if (align === "para") {
-        // para-augmented stuff have digonal prismatic symmetry
-        // TODO this doesn't count a square prism (cube)
-        return Dihedral.get(2, "prism")
-      } else {
-        // meta-augmented stuff has biradial symmetry
-        return Cyclic.biradial
-      }
-    }
-    case 3: {
-      // Triaugmented triangular/hexagonal prism has triangular prismatic symmetry
-      return Dihedral.get(3, "prism")
-    }
-    default: {
-      // for zero-counts, return the usual prism symmetry
-      return getPrismSymmetry(name)
-    }
-  }
-}
-
-function getAugmentedClassicalSymmetry(name: string) {
-  const { count, base, align } = augmentedClassicals.get(name)
-  switch (count) {
-    case 1:
-      return Cyclic.get(base)
-    case 2:
-      if (base === 4) {
-        return Dihedral.get(4, "prism")
-      }
-      return align === "para" ? Dihedral.get(5, "antiprism") : Cyclic.biradial
-    case 3:
-      return Cyclic.get(3)
-    default:
-      return getClassicalSymmetry(name)
-  }
-}
-
-function getDiminishedIcosahedronSymmetry(name: string) {
-  const { count, align } = diminishedIcosahedra.get(name)
-  switch (count) {
-    case 3:
-      return Cyclic.get(3)
-    case 2:
-      return align === "meta" ? Cyclic.biradial : Dihedral.get(5, "antiprism")
-    case 1:
-      return Cyclic.get(5)
-    default:
-      return getClassicalSymmetry(name)
-  }
-}
-
-function getRhombicosidodecahedraSymmetry(name: string) {
-  const { gyrate, diminished, align } = rhombicosidodecahedra.get(name)
-  // only gyrations or only diminishes
-  const pure = !gyrate || !diminished
-  switch (gyrate + diminished) {
-    case 0:
-      // normal rhombicosidodecahedron
-      return getClassicalSymmetry(name)
-    case 1:
-      // pentagonal pyramidal
-      return Cyclic.get(5)
-    case 2:
-      if (align === "para") {
-        return pure ? Dihedral.get(5, "antiprism") : Cyclic.get(5)
-      }
-      return pure ? Cyclic.biradial : Cyclic.bilateral
-    //
-    case 3:
-      return pure ? Cyclic.get(3) : Cyclic.bilateral
-    default:
-      throw new Error(
-        `Way too many changes to this polyhedron: gyrate=${gyrate}, diminished=${diminished}`,
-      )
-  }
-}
-
-function getSnubAntiprismSymmetry(name: string) {
-  const { base } = snubAntiprisms.get(name)
-  return Dihedral.get(base, "antiprism")
 }
 
 const elementaryMapping: Record<string, Symmetry> = {
@@ -150,36 +18,121 @@ const elementaryMapping: Record<string, Symmetry> = {
   "triangular hebesphenorotunda": Cyclic.get(3),
 }
 
+function getClassicalSymmetry(name: string): Symmetry {
+  throw new Error("Invalid polyhedron")
+}
+
 /**
  * Return the Symmetry of the given polyhedron name
  */
 export default function getSymmetry(name: string): Symmetry {
   assertValidSolid(name)
 
-  // TODO this is a lot of repeated patterns that may benefit from some sort of visitor pattern
-  if (classicals.hasName(name)) {
-    return getClassicalSymmetry(name)
-  }
-  if (prisms.hasName(name)) {
-    return getPrismSymmetry(name)
-  }
-  if (capstones.hasName(name)) {
-    return getCapstoneSymmetry(name)
-  }
-  if (augmentedPrisms.hasName(name)) {
-    return getAugmentedPrismSymmetry(name)
-  }
-  if (augmentedClassicals.hasName(name)) {
-    return getAugmentedClassicalSymmetry(name)
-  }
-  if (diminishedIcosahedra.hasName(name)) {
-    return getDiminishedIcosahedronSymmetry(name)
-  }
-  if (rhombicosidodecahedra.hasName(name)) {
-    return getRhombicosidodecahedraSymmetry(name)
-  }
-  if (snubAntiprisms.hasName(name)) {
-    return getSnubAntiprismSymmetry(name)
-  }
-  return elementaryMapping[name]
+  return visitTables(name, {
+    classicals({ family, operation }) {
+      return Polyhedral.get(groupNames[family], operation === "snub")
+    },
+    prisms({ base, type }) {
+      return Dihedral.get(base, type)
+    },
+    capstones({ base, type, count, elongation, gyrate }) {
+      // Single-count capstones all have pyramidal symmetry
+      if (count === 1) {
+        return Cyclic.get(base)
+      }
+      const gyroelongated = elongation === "antiprism"
+
+      if (type === "pyramid") {
+        return Dihedral.get(base, gyroelongated ? "antiprism" : "prism")
+      } else if (type === "cupolarotunda") {
+        return Cyclic.get(base, !!gyroelongated)
+      } else {
+        // cupolae and rotundae
+        const reflection = gyroelongated
+          ? undefined
+          : gyrate === "gyro"
+          ? "antiprism"
+          : "prism"
+        return Dihedral.get(base, reflection)
+      }
+    },
+    augmentedPrisms({ count, align }) {
+      switch (count) {
+        case 1:
+          // mono-augmented prisms all have biradial symmetry
+          return Cyclic.biradial
+        case 2:
+          // para-augmented stuff have digonal prismatic symmetry
+          // meta-augmented stuff has biradial symmetry
+          return align === "para" ? Dihedral.get(2, "prism") : Cyclic.biradial
+        case 3:
+          // Triaugmented triangular/hexagonal prism has triangular prismatic symmetry
+          return Dihedral.get(3, "prism")
+        default:
+          // for zero-counts, return the usual prism symmetry
+          throw new Error("reached invalid code")
+      }
+    },
+    augmentedClassicals({ count, base, align }) {
+      switch (count) {
+        case 1:
+          return Cyclic.get(base)
+        case 2:
+          if (base === 4) {
+            return Dihedral.get(4, "prism")
+          }
+          return align === "para"
+            ? Dihedral.get(5, "antiprism")
+            : Cyclic.biradial
+        case 3:
+          return Cyclic.get(3)
+        default:
+          throw new Error("reached invalid code")
+      }
+    },
+    diminishedIcosahedra({ count, align }) {
+      switch (count) {
+        case 3:
+          return Cyclic.get(3)
+        case 2:
+          return align === "meta"
+            ? Cyclic.biradial
+            : Dihedral.get(5, "antiprism")
+        case 1:
+          return Cyclic.get(5)
+        default:
+          throw new Error("Reached invalid code")
+      }
+    },
+    rhombicosidodecahedra({ gyrate, diminished, align }) {
+      // only gyrations or only diminishes
+      const pure = !gyrate || !diminished
+      switch (gyrate + diminished) {
+        case 0:
+          // normal rhombicosidodecahedron
+          return getClassicalSymmetry(name)
+        case 1:
+          // pentagonal pyramidal
+          return Cyclic.get(5)
+        case 2:
+          if (align === "para") {
+            return pure ? Dihedral.get(5, "antiprism") : Cyclic.get(5)
+          }
+          return pure ? Cyclic.biradial : Cyclic.bilateral
+        //
+        case 3:
+          return pure ? Cyclic.get(3) : Cyclic.bilateral
+        default:
+          throw new Error(
+            `Way too many changes to this polyhedron: gyrate=${gyrate}, diminished=${diminished}`,
+          )
+      }
+    },
+    snubAntiprisms({ base }) {
+      return Dihedral.get(base, "antiprism")
+    },
+    default() {
+      return elementaryMapping[name]
+    },
+  })
 }
