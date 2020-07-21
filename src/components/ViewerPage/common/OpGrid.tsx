e40c6023b2c48c69b08d5faedf5fe34b5f44e14b
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-07-20 20:12:13 -0700

    get rid of comments

--- src/components/ViewerPage/common/OpGrid.tsx
+++ src/components/ViewerPage/common/OpGrid.tsx
@@ -15,8 +15,6 @@ import {
 import OperationIcon from "./OperationIcon"
 
 const opLayout: OpName[][] = [
-  // ["truncate", "cotruncate", "sharpen", "cosharpen"],
-  // ["rectify", "rectify", "unrectify", "dual"],
   ["truncate", "rectify", "sharpen", "dual"],
   ["expand", "snub", "contract", "twist"],
   ["elongate", "gyroelongate", "shorten", "turn"],
