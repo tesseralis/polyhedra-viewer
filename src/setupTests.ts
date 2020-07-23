2e3bbf0407e5dd0df56686fecd21e69d6bbf4eff
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-07-23 00:15:05 -0700

    update yarn

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,10 +1,7 @@
 import "@testing-library/jest-dom"
-import { configure } from "enzyme"
-import Adapter from "enzyme-adapter-react-16"
 import { StyleSheetTestUtils } from "aphrodite"
 import "jest-extended"
 
-configure({ adapter: new Adapter() })
 StyleSheetTestUtils.suppressStyleInjection()
 
 jest.mock("x3domWrapper.ts")
