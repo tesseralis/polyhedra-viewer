92aaa7ae224237fc00f145c6429d0be2a3df37d8
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-07-31 21:15:25 -0700

    move truncate ops to a folder

---
+++ src/math/operations/truncateOps/truncateAugmented.ts
@@ -0,0 +1,77 @@
+import Composite from "data/specs/Composite"
+import { makeOpPair } from "../operationPairs"
+import { getCentroid } from "math/geom"
+import { getTransformedVertices } from "../operationUtils"
+import { AugmentedClassicalForme } from "math/formes/CompositeForme"
+import { find } from "utils"
+import { getSharpenPoint, getSharpenPointEdge } from "./truncateHelpers"
+
+export default makeOpPair<AugmentedClassicalForme>({
+  graph: function* () {
+    for (const entry of Composite.query.where(
+      (s) =>
+        s.isAugmentedClassical() &&
+        s.data.source.isClassical() &&
+        s.data.source.isRegular(),
+    )) {
+      yield {
+        left: entry,
+        right: entry.withData({
+          source: entry.sourceClassical().withOperation("truncate"),
+        }),
+      }
+    }
+  },
+  middle: "right",
+  getPose($, forme) {
+    const { specs } = forme
+    const caps = forme.caps()
+    // Calculate the centroid *only* for the source polyhedra
+    const centroid = forme.sourceCentroid()
+    const cap = caps[0]
+    const boundary = cap.boundary()
+
+    let crossAxis
+    if (specs.isTri()) {
+      // Use the midpoin of the normals of the two other caps
+      crossAxis = getCentroid([caps[1].normal(), caps[2].normal()])
+    } else if (specs.isBi() && specs.isMeta()) {
+      // If metabiaugmented, use the normal of the other cap
+      crossAxis = caps[1].normal()
+    } else {
+      crossAxis = find(boundary.edges, (e) => forme.isMainFace(e.twinFace()))
+        .midpoint()
+        .sub(boundary.centroid())
+    }
+
+    return {
+      origin: centroid,
+      scale: forme.mainFace().centroid().distanceTo(centroid),
+      orientation: [cap.normal(), crossAxis],
+    }
+  },
+  toLeft(forme) {
+    const truncatedFaces = forme.minorFaces()
+    // the inner faces of the caps
+    const cupolaFaces = forme.innerCapFaces()
+    return getTransformedVertices(
+      [...truncatedFaces, ...cupolaFaces],
+      (face) => {
+        if (cupolaFaces.some((f) => f.equals(face))) {
+          // Sharpen the cupola faces
+          const v = face.vertices[0]
+          // Find a triangular cupola face
+          const otherFace = find(
+            v.adjacentFaces(),
+            (f) => f.numSides === 3 && !f.equals(face),
+          )!
+
+          return getSharpenPoint(face, v.vec, otherFace.centroid())
+        } else {
+          const edge = find(face.edges, (e) => forme.isMainFace(e.twinFace()))
+          return getSharpenPointEdge(face, edge)
+        }
+      },
+    )
+  },
+})
---
+++ src/math/operations/truncateOps/truncateRegular.ts
@@ -0,0 +1,49 @@
+import { Facet } from "data/specs/Classical"
+import { getTransformedVertices, Pose } from "../operationUtils"
+import ClassicalForme from "math/formes/ClassicalForme"
+import { getSharpenPointEdge, makeTruncateTrio } from "./truncateHelpers"
+
+function getRegularPose(
+  forme: ClassicalForme,
+  facet: Facet = forme.specs.facet(),
+): Pose {
+  return {
+    origin: forme.geom.centroid(),
+    scale: forme.facetFace(facet).distanceToCenter(),
+    orientation: forme
+      .adjacentFacetFaces(facet)
+      .map((face) => face.normal()) as any,
+  }
+}
+
+/**
+ * Describes the truncation operations on a Platonic solid.
+ */
+export default makeTruncateTrio(
+  (forme, options) => getRegularPose(forme, options?.right?.facet),
+  {
+    left: {
+      operation: "regular",
+      transformer(forme) {
+        return getTransformedVertices(forme.minorFacetFaces(), (face) =>
+          getSharpenPointEdge(face, face.edges[0]),
+        )
+      },
+    },
+    middle: { operation: "truncate" },
+    right: {
+      operation: "rectify",
+      // The rectified version is the only thing we need to choose an option for
+      // when we move out of it
+      options: (entry) => ({ facet: entry.facet() }),
+      transformer(forme) {
+        // All edges that between two truncated faces
+        const edges = forme.geom.edges.filter((e) =>
+          e.adjacentFaces().every((f) => forme.isMainFacetFace(f)),
+        )
+        // Move each edge to its midpoint
+        return getTransformedVertices(edges, (e) => e.midpoint())
+      },
+    },
+  },
+)
