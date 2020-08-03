db045948ae8b9eb5c9226047e2194b360b1b72a1
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2020-08-03 00:01:44 -0700

    yay the edges work!

--- src/components/ViewerPage/common/SolidScene/ThreePolyhedron.tsx
+++ src/components/ViewerPage/common/SolidScene/ThreePolyhedron.tsx
@@ -1,7 +1,14 @@
 import { zip } from "lodash"
-import React, { useRef } from "react"
-import { DoubleSide, Vector3, Face3, Color } from "three"
-import { useUpdate } from "react-three-fiber"
+import React, { useRef, useMemo } from "react"
+import {
+  BufferAttribute,
+  DoubleSide,
+  Vector3,
+  Face3,
+  Color,
+  BufferGeometry,
+} from "three"
+import { useFrame, useUpdate } from "react-three-fiber"
 
 function convertVertex([x, y, z]: [number, number, number]) {
   return new Vector3(x, y, z)
@@ -35,7 +42,7 @@ export default function ThreePolyhedron({
 }: any) {
   // This reference will give us direct access to the mesh
   const mesh = useRef<any>()
-  const { vertices, faces } = value
+  const { vertices, faces, edges } = value
   const hit = useRef(new Vector3())
 
   const ref = useUpdate(
@@ -50,33 +57,64 @@ export default function ThreePolyhedron({
     [vertices, faces, colors],
   )
 
+  const edgeGeom = useMemo(() => {
+    const geom = new BufferGeometry()
+    const positions = new Float32Array(200 * 3) // 3 vertices per point
+    geom.setAttribute("position", new BufferAttribute(positions, 3))
+    return geom
+  }, [])
+
+  useFrame(() => {
+    const positions = edgeGeom.attributes.position.array as number[]
+    edges.forEach((edge: any, i: number) => {
+      const [i1, i2] = edge
+      const v1 = vertices[i1]
+      const v2 = vertices[i2]
+
+      positions[i * 6 + 0] = v1[0]
+      positions[i * 6 + 1] = v1[1]
+      positions[i * 6 + 2] = v1[2]
+      positions[i * 6 + 3] = v2[0]
+      positions[i * 6 + 4] = v2[1]
+      positions[i * 6 + 5] = v2[2]
+    })
+    edgeGeom.setDrawRange(0, edges.length * 2)
+    edgeGeom.attributes.position.needsUpdate = true
+  })
+
   return (
-    <mesh
-      ref={mesh}
-      scale={[1, 1, 1]}
-      onPointerDown={(e) => {
-        // FIXME it's still pretty finnicky..
-        hit.current.copy(e.point)
-      }}
-      onPointerUp={(e) => {
-        if (!hit.current.equals(e.point)) return
-        onClick?.(e.point.toArray())
-      }}
-      onPointerMove={(e) => {
-        hit.current.copy(e.point)
-        onPointerMove?.(e.point.toArray())
-      }}
-      onPointerOut={(e) => {
-        onPointerOut?.(e.point.toArray())
-      }}
-    >
-      <geometry ref={ref} attach="geometry" />
-      <meshStandardMaterial
-        side={DoubleSide}
-        attach="material"
-        color="grey"
-        args={[{ vertexColors: true }]}
-      />
-    </mesh>
+    <>
+      <mesh
+        ref={mesh}
+        scale={[1, 1, 1]}
+        onPointerDown={(e) => {
+          // FIXME it's still pretty finnicky..
+          hit.current.copy(e.point)
+        }}
+        onPointerUp={(e) => {
+          if (!hit.current.equals(e.point)) return
+          onClick?.(e.point.toArray())
+        }}
+        onPointerMove={(e) => {
+          hit.current.copy(e.point)
+          onPointerMove?.(e.point.toArray())
+        }}
+        onPointerOut={(e) => {
+          onPointerOut?.(e.point.toArray())
+        }}
+      >
+        <geometry ref={ref} attach="geometry" />
+        <meshStandardMaterial
+          side={DoubleSide}
+          attach="material"
+          color="grey"
+          args={[{ vertexColors: true }]}
+        />
+      </mesh>
+      <lineSegments geometry={edgeGeom}>
+        {/* <bufferGeometry ref={edgeRef} attach="geometry" /> */}
+        <lineBasicMaterial color={0x222222} linewidth={10} />
+      </lineSegments>
+    </>
   )
 }
