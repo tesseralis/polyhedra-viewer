ce00a1597fddbd7a329678b642c8793e80d8407c
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-12-14 00:56:02 -0800

    rearrange group layout for later

---
+++ pages/[polyhedron]/GroupLayout.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import { Canvas } from "@react-three/fiber"
+import { useStyle } from "styles"
+import { OrthographicCamera, TrackballControls } from "@react-three/drei"
+import { useRouter } from "next/router"
+
+export default function GroupLayout({ children }: any) {
+  const router = useRouter()
+  const style = useStyle({
+    position: "absolute",
+    backgroundColor: "#111",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  })
+  return (
+    <div {...style()}>
+      <Canvas>
+        <OrthographicCamera makeDefault position={[0, 0, 5]} zoom={20}>
+          {[]}
+        </OrthographicCamera>
+        <directionalLight position={[0, 0.5, 1]} />
+        <TrackballControls enabled noRotate panSpeed={5} />
+        <group>{children(router)}</group>
+      </Canvas>
+    </div>
+  )
+}
--- pages/[polyhedron]/index.page.tsx
+++ pages/[polyhedron]/index.page.tsx
@@ -1,7 +1,22 @@
-import GroupPage from "./GroupPage"
+import GroupLayout from "./GroupLayout"
+import { tableSections } from "lib/tables"
+import PolyhedronTable from "components/MuseumPage/PolyhedronTable"
 
 export default function Page({ group }: any) {
-  return <GroupPage group={group} />
+  const table = tableSections.find((table) => table.id === group)!
+  return (
+    <GroupLayout>
+      {(router: any) => {
+        return table.tables?.map((table, j) => {
+          return (
+            <group key={table.caption} position={[j * subsecSpacing, 0, 0]}>
+              <PolyhedronTable navigate={router} table={table} />
+            </group>
+          )
+        })
+      }}
+    </GroupLayout>
+  )
 }
 
 export const getServerSideProps = async ({ params }: any) => {
@@ -18,3 +33,7 @@ export const getServerSideProps = async ({ params }: any) => {
 }
 
 const groups = ["uniform", "capstones", "composite", "elementary"]
+
+// TODO deduplicate with the controls in the other scene
+const sectionSpacing = 50
+const subsecSpacing = 20
