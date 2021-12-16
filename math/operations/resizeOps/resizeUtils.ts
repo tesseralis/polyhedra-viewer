5cfd957cd39dc6cf7ee6f08361f618c984caefd6
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-12-16 03:59:20 -0800

    simplify API

--- math/operations/resizeOps/resizeUtils.ts
+++ math/operations/resizeOps/resizeUtils.ts
@@ -5,7 +5,6 @@ import { isCodirectional } from "math/geom"
 import { PolyhedronForme } from "math/formes"
 
 type GetFaces<Forme> = (forme: Forme) => Face[]
-type GetIsExact<Forme> = (forme: Forme) => boolean
 
 /**
  * Return the expanded vertices of the polyhedron resized to the given distance-from-center
@@ -13,20 +12,14 @@ type GetIsExact<Forme> = (forme: Forme) => boolean
  */
 export function getResizeFunction<Forme extends PolyhedronForme>(
   getFacesToMap: GetFaces<Forme>,
-  getIsExact: GetIsExact<Forme> = () => true,
 ) {
   return function getResizedVertices(forme: Forme, result: Forme) {
-    const isExact = getIsExact(forme)
-    const facePairs = getFacePairs(
-      forme.geom.faces,
-      getFacesToMap(result),
-      isExact,
-    )
+    const facePairs = getFacePairs(forme.geom.faces, getFacesToMap(result))
 
     // create a map from the initial vertices to the end vertices
     const mapping: Vertex[] = []
     for (const [f1, f2] of facePairs) {
-      for (const [v1, v2] of getVertexPairs(f1, f2, isExact)) {
+      for (const [v1, v2] of getVertexPairs(f1, f2)) {
         mapping[v1.index] = v2
       }
     }
@@ -38,56 +31,34 @@ export function getResizeFunction<Forme extends PolyhedronForme>(
 }
 
 // Find the faces in the first set that map onto the second set
-function getFacePairs(first: Face[], second: Face[], isSnub?: boolean) {
+function getFacePairs(first: Face[], second: Face[]) {
   return second.map((face) => {
-    return [findFacePartner(face, first, isSnub), face]
+    return [findFacePartner(face, first), face]
   })
 }
 
-function findFacePartner(face: Face, candidates: Face[], isSnub?: boolean) {
-  if (isSnub) {
-    return minBy(candidates, (face2) => {
-      return face.normal().angleTo(face2.normal())
-    })!
-  } else {
-    const partner = candidates.find((face2) =>
-      isCodirectional(face.normal(), face2.normal()),
-    )
-    if (!partner) {
-      throw new Error("Something went wrong finding a partner face")
-    }
-    return partner
-  }
+function findFacePartner(face: Face, candidates: Face[]) {
+  return minBy(candidates, (face2) => {
+    return face.normal().angleTo(face2.normal())
+  })!
 }
 
-function getVertexPairs(f1: Face, f2: Face, isSnub?: boolean) {
-  const partnerIndex = getPartnerVertexIndex(f1, f2, isSnub)
+function getVertexPairs(f1: Face, f2: Face) {
+  const partnerIndex = getPartnerVertexIndex(f1, f2)
   return f1.vertices.map((v, i) => {
     return [v, getCyclic(f2.vertices, i + partnerIndex)] as [Vertex, Vertex]
   })
 }
 
-function getPartnerVertexIndex(f1: Face, f2: Face, isSnub?: boolean) {
+function getPartnerVertexIndex(f1: Face, f2: Face) {
   const v0 = f1.vertices[0]
-  if (isSnub) {
-    // snub vertices aren't aligned, so get the closest one
-    return minBy(range(0, f2.numSides), (i) => {
-      const v = f2.vertices[i]
-      return f1
-        .centroid()
-        .clone()
-        .sub(v0.vec)
-        .angleTo(f2.centroid().clone().sub(v.vec))
-    })!
-  }
-  const partnerIndex = f2.vertices.findIndex((v) =>
-    isCodirectional(
-      f1.centroid().clone().sub(v0.vec),
-      f2.centroid().clone().sub(v.vec),
-    ),
-  )
-  if (partnerIndex < 0) {
-    throw new Error("Something went wrong finding a partner vertex")
-  }
-  return partnerIndex
+  // snub vertices aren't aligned, so get the closest one
+  return minBy(range(0, f2.numSides), (i) => {
+    const v = f2.vertices[i]
+    return f1
+      .centroid()
+      .clone()
+      .sub(v0.vec)
+      .angleTo(f2.centroid().clone().sub(v.vec))
+  })!
 }
