8ec1482048814523c63ecc9e96dc57ada4c37c3f
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-12-16 03:35:19 -0800

    convert resizeClassical to be vertex map based

--- math/operations/resizeOps/resizeClassical.ts
+++ math/operations/resizeOps/resizeClassical.ts
@@ -1,11 +1,10 @@
+import { minBy, range } from "lodash-es"
+import { getCyclic } from "lib/utils"
 import { Classical, FacetType, twists, oppositeTwist } from "specs"
 import { makeOpPair, OpPairInput, GraphOpts } from "../operationPairs"
-import {
-  getTransformedVertices,
-  FacetOpts,
-  TwistOpts,
-  Pose,
-} from "../operationUtils"
+import { FacetOpts, TwistOpts, Pose } from "../operationUtils"
+import { isCodirectional } from "math/geom"
+import { Vertex, Face } from "math/polyhedra"
 import { ClassicalForme } from "math/formes"
 
 /**
@@ -16,19 +15,27 @@ import { ClassicalForme } from "math/formes"
  * @param distance the normalized distance from center to put those faces
  * @param angle the angle to twist the faces by
  */
-function getResizedVertices(
-  start: ClassicalForme,
-  facet: FacetType,
-  result: ClassicalForme,
-) {
-  const angle = start.snubAngle(facet)
-  const distance = result.inradius(facet) / result.geom.edgeLength()
-  const scale = start.geom.edgeLength() * distance - start.inradius(facet)
-  return getTransformedVertices(start.facetFaces(facet), (f) => {
-    const rotateM = f.rotateNormal(angle)
-    const translateM = f.translateNormal(scale)
-    return f.withCentroidOrigin(rotateM.premultiply(translateM))
+function getResizedVertices(forme: ClassicalForme, result: ClassicalForme) {
+  // FIXME deduplicate with `resizePyramid`
+  // (everything is the same)
+  const isSnub = forme.specs.isSnub()
+  const facePairs = getFacePairs(
+    forme.geom.faces,
+    getFacesToMap(result),
+    isSnub,
+  )
+
+  // create a map from the initial vertices to the end vertices
+  const mapping: Vertex[] = []
+  for (const [f1, f2] of facePairs) {
+    for (const [v1, v2] of getVertexPairs(f1, f2, isSnub)) {
+      mapping[v1.index] = v2
+    }
+  }
+  const res = forme.geom.vertices.map((v) => {
+    return mapping[v.index]
   })
+  return res
 }
 
 function getClassicalPose(forme: ClassicalForme, facet: FacetType): Pose {
@@ -52,8 +59,8 @@ function getResizeArgs<L, R>(
     getPose(forme, options) {
       return getClassicalPose(forme, getFacet(options))
     },
-    toLeft(forme, options, result) {
-      return getResizedVertices(forme, getFacet(options), result)
+    toLeft(forme, $, result) {
+      return getResizedVertices(forme, result)
     },
   }
 }
@@ -121,3 +128,68 @@ export const twist = makeOpPair<Classical, TwistOpts, {}>({
     }
   },
 })
+
+// get the result faces to map the start faces too
+function getFacesToMap(result: ClassicalForme) {
+  if (result.specs.isRegular()) {
+    return result.geom.faces
+  }
+  // for a twist operation, the "end" is a cantellated solid
+  // so return all facet-faces.
+  return [...result.facetFaces("face"), ...result.facetFaces("vertex")]
+}
+
+// Find the faces in the first set that map onto the second set
+function getFacePairs(first: Face[], second: Face[], isSnub?: boolean) {
+  return second.map((face) => {
+    return [findFacePartner(face, first, isSnub), face]
+  })
+}
+
+function findFacePartner(face: Face, candidates: Face[], isSnub?: boolean) {
+  if (isSnub) {
+    return minBy(candidates, (face2) => {
+      return face.normal().angleTo(face2.normal())
+    })!
+  } else {
+    const partner = candidates.find((face2) =>
+      isCodirectional(face.normal(), face2.normal()),
+    )
+    if (!partner) {
+      throw new Error("Something went wrong finding a partner face")
+    }
+    return partner
+  }
+}
+
+function getVertexPairs(f1: Face, f2: Face, isSnub?: boolean) {
+  const partnerIndex = getPartnerVertexIndex(f1, f2, isSnub)
+  return f1.vertices.map((v, i) => {
+    return [v, getCyclic(f2.vertices, i + partnerIndex)] as [Vertex, Vertex]
+  })
+}
+
+function getPartnerVertexIndex(f1: Face, f2: Face, isSnub?: boolean) {
+  const v0 = f1.vertices[0]
+  if (isSnub) {
+    // snub vertices aren't aligned, so get the closest one
+    return minBy(range(0, f2.numSides), (i) => {
+      const v = f2.vertices[i]
+      return f1
+        .centroid()
+        .clone()
+        .sub(v0.vec)
+        .angleTo(f2.centroid().clone().sub(v.vec))
+    })!
+  }
+  const partnerIndex = f2.vertices.findIndex((v) =>
+    isCodirectional(
+      f1.centroid().clone().sub(v0.vec),
+      f2.centroid().clone().sub(v.vec),
+    ),
+  )
+  if (partnerIndex < 0) {
+    throw new Error("Something went wrong finding a partner vertex")
+  }
+  return partnerIndex
+}
