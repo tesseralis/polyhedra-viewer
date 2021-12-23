454f3eec02e64d84dc566a8f51007d0a70a93359
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-12-22 20:18:41 -0800

    update resizePrism and resizePyramid

--- math/operations/resizeOps/resizePrism.ts
+++ math/operations/resizeOps/resizePrism.ts
@@ -4,39 +4,7 @@ import { Pose, FacetOpts } from "../operationUtils"
 import { CapstoneForme } from "math/formes"
 import { getMorphFunction } from "../morph"
 
-const getResizedVertices = getMorphFunction()
-
-function getCapstoneCrossAxis(forme: CapstoneForme) {
-  const topBoundary = forme.endBoundaries()[0]
-  // for the bicupolae, find an edge connected to a square cupola face
-  if (forme.specs.isSecondary()) {
-    return topBoundary.edges.find((e) => e.face.numSides === 4)!
-  }
-  // For prisms, it can be any edge
-  if (forme.specs.isPrismatic()) {
-    return topBoundary.edges[0]
-  }
-  // For bipyramids, it's vertex-centered
-  return topBoundary.vertices[0]
-}
-
-function getPose(forme: CapstoneForme): Pose {
-  const top = forme.ends()[0]
-  const crossAxis = getCapstoneCrossAxis(forme)
-  return {
-    origin: forme.origin(),
-    scale: forme.geom.edgeLength(),
-    orientation: [top, crossAxis],
-  }
-}
-
-function* elongatedOrthobicupolae() {
-  for (const specs of Capstone.query.where(
-    (cap) => cap.isCupola() && cap.isBi() && cap.isElongated() && cap.isOrtho(),
-  )) {
-    yield specs
-  }
-}
+const morph = getMorphFunction()
 
 export const expand = makeOpPair<Capstone, {}, FacetOpts>({
   graph: function* () {
@@ -61,7 +29,7 @@ export const expand = makeOpPair<Capstone, {}, FacetOpts>({
   },
   middle: "right",
   getPose,
-  toLeft: getResizedVertices,
+  toLeft: morph,
 })
 
 export const dual = makeOpPair<Capstone>({
@@ -80,6 +48,38 @@ export const dual = makeOpPair<Capstone>({
       gyrate: "ortho",
     }),
   getPose,
-  toLeft: getResizedVertices,
-  toRight: getResizedVertices,
+  toLeft: morph,
+  toRight: morph,
 })
+
+function getPose(forme: CapstoneForme): Pose {
+  const top = forme.ends()[0]
+  const crossAxis = getCapstoneCrossAxis(forme)
+  return {
+    origin: forme.origin(),
+    scale: forme.geom.edgeLength(),
+    orientation: [top, crossAxis],
+  }
+}
+
+function getCapstoneCrossAxis(forme: CapstoneForme) {
+  const topBoundary = forme.endBoundaries()[0]
+  // for the bicupolae, find an edge connected to a square cupola face
+  if (forme.specs.isSecondary()) {
+    return topBoundary.edges.find((e) => e.face.numSides === 4)!
+  }
+  // For prisms, it can be any edge
+  if (forme.specs.isPrismatic()) {
+    return topBoundary.edges[0]
+  }
+  // For bipyramids, it's vertex-centered
+  return topBoundary.vertices[0]
+}
+
+function* elongatedOrthobicupolae() {
+  for (const specs of Capstone.query.where(
+    (cap) => cap.isCupola() && cap.isBi() && cap.isElongated() && cap.isOrtho(),
+  )) {
+    yield specs
+  }
+}
--- math/operations/resizeOps/resizePyramid.ts
+++ math/operations/resizeOps/resizePyramid.ts
@@ -1,13 +1,77 @@
 import { Capstone } from "specs"
-import { makeOpPair, OpPairInput } from "../operationPairs"
+import { makeOpPair } from "../operationPairs"
 import { Pose } from "../operationUtils"
 import { Face, Edge } from "math/polyhedra"
 import { CapstoneForme } from "math/formes"
 import { getMorphFunction } from "../morph"
 
-const getResizedVertices = getMorphFunction(getFacesToMap)
+export const expand = makeOpPair<Capstone>({
+  graph: function* () {
+    for (const entry of Capstone.query.where(
+      (c) => c.isPyramid() && c.isMono() && c.isShortened(),
+    )) {
+      yield {
+        left: entry,
+        right: entry.withData({ type: "secondary", count: 2, gyrate: "gyro" }),
+      }
+    }
+  },
+  middle: "right",
+  getPose,
+  toLeft: getMorphFunction(),
+})
 
-function getCapstonePose(forme: CapstoneForme): Pose {
+export const snub = makeOpPair<Capstone>({
+  graph: function* () {
+    for (const entry of Capstone.query.where(
+      (c) =>
+        c.isPyramid() && c.isMono() && c.isShortened() && !c.isPentagonal(),
+    )) {
+      yield {
+        left: entry,
+        right: entry.withElongation("snub"),
+      }
+    }
+  },
+  middle: "right",
+  getPose,
+  toLeft: getMorphFunction(),
+})
+
+export const twist = makeOpPair<Capstone>({
+  graph: function* () {
+    for (const entry of Capstone.query.where(
+      (c) =>
+        c.isCupola() &&
+        c.isGyro() &&
+        c.isBi() &&
+        c.isShortened() &&
+        !c.isPentagonal(),
+    )) {
+      yield {
+        left: entry,
+        right: entry.withElongation("snub"),
+      }
+    }
+  },
+  middle: "right",
+  getPose,
+  toLeft: getMorphFunction((result) => {
+    // The end result is the gyrobicupola, so only track the triangular faces of the caps.
+    return result.caps().flatMap((cap) => {
+      const items = cap
+        .boundary()
+        .adjacentFaces()
+        .filter((f) => f.numSides === 3)
+      if (!result.specs.isDigonal()) {
+        items.push(cap.topFace())
+      }
+      return items
+    })
+  }),
+})
+
+function getPose(forme: CapstoneForme): Pose {
   const top = forme.ends()[0]
   return {
     origin: forme.origin(),
@@ -27,84 +91,4 @@ function getCrossAxis(forme: CapstoneForme) {
     const top = forme.caps()[0]
     return top.boundary().edges.find((e) => e.face.numSides === 3)!
   }
-}
-
-type ResizeArgs<L, R> = Omit<OpPairInput<Capstone, L, R>, "graph">
-
-function getResizeArgs<L, R>(): ResizeArgs<L, R> {
-  return {
-    middle: "right",
-    getPose(forme) {
-      return getCapstonePose(forme)
-    },
-    toLeft: getResizedVertices,
-  }
-}
-
-const resizeArgs = getResizeArgs()
-
-export const expand = makeOpPair<Capstone>({
-  ...resizeArgs,
-  graph: function* () {
-    for (const entry of Capstone.query.where(
-      (c) => c.isPyramid() && c.isMono() && c.isShortened(),
-    )) {
-      yield {
-        left: entry,
-        right: entry.withData({ type: "secondary", count: 2, gyrate: "gyro" }),
-      }
-    }
-  },
-})
-
-export const snub = makeOpPair<Capstone>({
-  ...resizeArgs,
-  graph: function* () {
-    for (const entry of Capstone.query.where(
-      (c) =>
-        c.isPyramid() && c.isMono() && c.isShortened() && !c.isPentagonal(),
-    )) {
-      yield {
-        left: entry,
-        right: entry.withElongation("snub"),
-      }
-    }
-  },
-})
-
-export const twist = makeOpPair<Capstone>({
-  ...getResizeArgs(),
-  graph: function* () {
-    for (const entry of Capstone.query.where(
-      (c) =>
-        c.isCupola() &&
-        c.isGyro() &&
-        c.isBi() &&
-        c.isShortened() &&
-        !c.isPentagonal(),
-    )) {
-      yield {
-        left: entry,
-        right: entry.withElongation("snub"),
-      }
-    }
-  },
-})
-
-// get the result faces to map the start faces too
-function getFacesToMap(result: CapstoneForme) {
-  if (result.specs.isPyramid()) {
-    return result.geom.faces
-  }
-  // for a twist operation, the end result is the bicupola
-  return result.caps().flatMap((cap) => {
-    const items = cap
-      .boundary()
-      .adjacentFaces()
-      .filter((f) => f.numSides === 3)
-    if (!result.specs.isDigonal()) {
-      items.push(cap.topFace())
-    }
-    return items
-  })
 }
