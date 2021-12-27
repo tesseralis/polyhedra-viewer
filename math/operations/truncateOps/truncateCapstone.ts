fb0e9acf0c91e781d68405ff10333e426d593384
Nat Alison <1278991+tesseralis@users.noreply.github.com>
2021-12-27 12:37:27 -0800

    make it a new thing

--- math/operations/truncateOps/truncateCapstone.ts
+++ math/operations/truncateOps/truncateCapstone.ts
@@ -1,9 +1,9 @@
 import { createForme } from "math/formes"
 import { Capstone } from "specs"
-import { getMorphFunction } from "../morph"
 import { makeOpPair } from "../operationPairs"
 import { getGeometry } from "../operationUtils"
 import { rawTruncate } from "./truncateHelpers"
+import { CapstoneForme } from "math/formes"
 
 export const rectify = makeOpPair<Capstone>({
   graph: function* () {
@@ -17,23 +17,36 @@ export const rectify = makeOpPair<Capstone>({
     }
   },
   intermediate(entry) {
-    return createForme(entry.left, rawTruncate(getGeometry(entry.left)))
+    return new TruncatedPyramidForme(
+      entry.left,
+      rawTruncate(getGeometry(entry.left)),
+    )
   },
   getPose(forme, $, side) {
-    if (side !== "intermediate") {
-      return {
-        origin: forme.origin(),
-        scale: forme.geom.faces[0].distanceToCenter(),
-        orientation: forme.orientation(),
-      }
-    }
     return {
-      origin: forme.geom.centroid(),
-      scale: forme.geom.faces[0].distanceToCenter(),
-      // just do randomly for now
-      orientation: [forme.geom.faces[0], forme.geom.faces[1]],
+      origin: forme.origin(),
+      scale: forme.ends()[0].distanceToCenter(),
+      orientation: forme.orientation(),
     }
   },
   //   toLeft: getMorphFunction(),
   //   toRight: getMorphFunction(),
 })
+
+class TruncatedPyramidForme extends CapstoneForme {
+  orientation() {
+    const top = this.endBoundaries()[0]
+    return [top, top.edges.find((e) => e.twinFace().numSides !== 3)!] as const
+  }
+
+  *queryTops() {
+    const n = this.geom.largestFace().numSides
+    if (n === 6) {
+      const face = this.geom.largestFace()
+      const opposite = this.geom.faces.find((f) => f.isInverse(face))!
+      yield face
+      yield opposite
+    }
+    yield* this.geom.facesWithNumSides(n)
+  }
+}
